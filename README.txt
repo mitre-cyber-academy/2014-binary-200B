d96a25efc496f39f0073689fcce3527cb81200c5
Nicholas Magulick <nmagulick@mitre.org>
2014-06-17 09:24:07 -0400

    Updated README with compilation instructions. Also edited code to make it solvable.

--- README.txt
+++ README.txt
@@ -3,6 +3,8 @@ Name: Switch Disassembly
 Description: User will be presented with only an executable file. User will then have to use a debugger to disassemble the code in order to see what the code is doing.
 Once the code is translated, the user will have to find the the correct combination of characters to get the correct key.
 
-Solution: The correct ordering of characters is: 045216873!
+Solution: The correct ordering of characters is: 0452168739
+
+To compile: gcc -m32  -Wall -lssl -lcrypto switch-disassemble.c -o switch
 
 Key: MCA-5FCB1730
--- switch-disassemble.c
+++ switch-disassemble.c
@@ -2,52 +2,108 @@
 /* sends "MCA-" plus 8 hex digits to terminal if user enters "012345678" at keyboard */
 /* To be provided in executable form only.  Can be made more challenging by randomizing the case order */
 #include <stdio.h>
+#include <stdbool.h>
+#include <ctype.h>
+#include <string.h>
+#include <stdlib.h>
+#include <openssl/md5.h>
+
+bool isCorrect(int a[], int n);
 
 int main()
 {
+  int arr[10] = {0};
+  bool pass = 0;
+  int i = 0;
   char check;
+
+  puts("Enter ten characters...");
   
   do {
     check = getchar();
-    switch (check)
+
+    if(isdigit(check) && i < 10)
     {
-      case '0':
-        printf("MCA-");
-        break;
-      case '1':
-        printf("B"); /* first of 8 hex digits*/
-        break;
-      case '2':
-        printf("C");
-        break;
-      case '3':
-        printf("0");
-        break;
-      case '4':
-        printf("5");
-        break;
-      case '5':
-        printf("F");
-        break;
-      case '6':
-        printf("1");
-        break;
-      case '7':
-        printf("3");
-        break;
-      case '8':
-        printf("7");
-        break;
-      case '!':
-        printf(" Done\n");
-        break;
-      default:
-        printf(" Oops\n");
-        break;
+        arr[i] = atoi(&check);
+        i++;
     }
-  }while (check != '!');
+
+    if(i >= 10)
+    {
+      for(int j = 0; j < 10; j++)
+      {
+        switch (arr[j])
+        {
+          case 0:
+            printf("MCA-");
+            break;
+          case 1:
+            printf("B"); 
+            break;
+          case 2:
+            printf("C");
+            break;
+          case 3:
+            printf("0");
+            break;
+          case 4:
+            printf("5");
+            break;
+          case 5:
+            printf("F");
+            break;
+          case 6:
+            printf("1");
+            break;
+          case 7:
+            printf("3");
+            break;
+          case 8:
+            printf("7");
+            break;
+          case 9:
+            printf(" Done\n");
+            pass = isCorrect(arr, 9);
+            break;
+          default:
+            printf("What?");
+            break;
+        }
+        
+        i = 0;
+
+      }
+    }
+
+  }while (pass == 0);
+
+  return(0);
 }
 
 
+bool isCorrect(int a[], int n)
+{
+  char combo[n+1];
+  unsigned char md5sum[MD5_DIGEST_LENGTH];
+
+  for(int i = 0; i < n; i++)
+  {
+    combo[i] = a[i] + '0';
+  }
+
+  MD5(combo, strlen(combo), md5sum);
+
+  if(strcmp(md5sum, "\x08\xf2\x4a\xa4\x7e\xa2\xad\xd2\x7b\x73\xe3\x3b\x0f\x8e\xe2\xf3") == 0)
+  {
+    puts("Granted!");
+    return 1;
+  }
+  else
+  {
+    puts("NO");
+    return 0;
+  }
+}
+
 //Key: MCA-5FC91730
-//Correct input: 045216873!
+//Correct input: 0452168739
